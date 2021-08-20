5d00ed77e24738e81174ed337ad8a8a3f70205e5
Juan Vazquez <ehulises28@hotmail.com>
2021-08-20 11:25:51 -0700

    solved algorithms

--- week 2/03-java Algorithms/03-log-nums/Unsolved/log-nums.js
+++ week 2/03-java Algorithms/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,8 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+function logNums(num) {
+  for (i = 1; i <= num; i++) {
+    console.log(i);
+  }
+}
