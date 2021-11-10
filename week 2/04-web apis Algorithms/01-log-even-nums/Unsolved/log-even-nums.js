2b48aba026dcce54b58210b80e9c20bf6831aaf4
Juan Vazquez <ehulises28@hotmail.com>
2021-11-10 10:17:15 -0800

    pushing solutions for a few algorithms

--- week 2/04-web apis Algorithms/01-log-even-nums/Unsolved/log-even-nums.js
+++ week 2/04-web apis Algorithms/01-log-even-nums/Unsolved/log-even-nums.js
@@ -1,4 +1,9 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
-
-var logEvenNums = function(num) {};
+function logEvenNums(num) {
+  for (i = 0; i <= num; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+}
